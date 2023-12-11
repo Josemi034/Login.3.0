d838287f803cece6b1d3c3cd20b66b6bba1929a4
Josemi034 <josemiguelbaezmendez120@gmail.com>
2023-12-11 13:41:36 -0400

    AB

---
+++ Login.3.0/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using inicio_de_seccion;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login._3._0
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
